df30dd1ee39d83af9031a1e88634132b5251918f
Singlerr <33392666+Singlerr@users.noreply.github.com>
2022-03-08 12:51:33 +0900

    Delete Assets/ScriptEngine directory

--- Assets/ScriptEngine/Elements/Nodes/ElementNode.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ScriptEngine.Elements.Nodes
-{
-    public class ElementNode : Node
-    {
-        public ElementNode(NodeType nodeType, string value) : base(nodeType, value)
-        {
-        }
-    }
-}
